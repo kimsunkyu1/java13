a0441c359bc40efff1bc7f743cc7642415bd6560
ksk <ksk@naver.com>
2019-05-28 17:38:05 +0900

    master revise animal

--- src/iducs/java/b/Animals.java
+++ src/iducs/java/b/Animals.java
@@ -3,6 +3,7 @@ package iducs.java.b;
 public interface Animals {
 	//public final double Height = 172.3;
 	String species = "동물"; //앞에 public final 생략되어있음.
+	String common = "공통점"; //앞에 public final 생략되어있음.
 	//public final String species = "동물"; //앞에 public final 생략되어있음.
 
 	
