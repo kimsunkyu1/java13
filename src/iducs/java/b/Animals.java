b85d5692505fbb5d554922d239e8a62b28327fe0
ksk <ksk@naver.com>
2019-05-28 17:34:30 +0900

    branch commit

--- src/iducs/java/b/Animals.java
+++ src/iducs/java/b/Animals.java
@@ -5,6 +5,7 @@ public interface Animals {
 	String species = "동물"; //앞에 public final 생략되어있음.
 	//public final String species = "동물"; //앞에 public final 생략되어있음.
 
+	
 	void move(); // 앞에 public abstract 생략되어있음
 	void eat();
 	void sound();
